f04c3b8e4fe74425375cc61c4c9457a4be19e2bf
Daniel Joshua Anyinu <anyinu.d@gmail.com>
2022-05-09 11:31:32 +0100

    Initial Commit

---
+++ src/pages/create/Create.jsx
@@ -0,0 +1,12 @@
+// Styles
+import './Create.css'
+
+
+
+function Create() {
+  return (
+    <div>Create</div>
+  )
+}
+
+export default Create
---
+++ src/pages/recipe/Recipe.jsx
@@ -0,0 +1,11 @@
+import './Recipe.css'
+
+
+
+function Recipe() {
+  return (
+    <div>Recipe</div>
+  )
+}
+
+export default Recipe
